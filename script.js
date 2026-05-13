f85f9841323713d14dc53015c18e08948edc477a
JustInCase-pdf <justin.huynh16@education.nsw.gov.au>
2026-05-13 12:00:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function changeMessage() {
-  document.getElementById("message").innerText = "You clicked the button! 🎉";
+let cartItems = [];
+
+function addItem(item) {
+  cartItems.push(item);
+  alert(item + " added to cart!");
+  localStorage.setItem("cart", JSON.stringify(cartItems));
 }
 
-function saveContactInfo() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  const data = `Name: ${name}\nEmail: ${email}\nMessage: ${message}\n\n`;
-  const blob = new Blob([data], { type: "text/plain" });
-  const link = document.createElement("a");
-  link.href = URL.createObjectURL(blob);
-  link.download = "contact-info.txt";
-  link.click();
+function payNow() {
+  alert("Payment successful! 🎉");
+  localStorage.removeItem("cart");
+  document.getElementById("cart").innerHTML = "";
 }
+
+window.onload = function() {
+  if (document.getElementById("cart")) {
+    const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    document.getElementById("cart").innerHTML = savedCart.map(i => `<li>${i}</li>`).join("");
+  }
+};

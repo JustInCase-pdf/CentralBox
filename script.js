595faad816632c286d7484938c1fc3dc565d6109
JustInCase-pdf <justin.huynh16@education.nsw.gov.au>
2026-05-13 11:33:47 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function changeMessage() {
   document.getElementById("message").innerText = "You clicked the button! 🎉";
 }
+
+function saveContactInfo() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const data = `Name: ${name}\nEmail: ${email}\nMessage: ${message}\n\n`;
+  const blob = new Blob([data], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "contact-info.txt";
+  link.click();
+}
